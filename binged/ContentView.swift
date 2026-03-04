69e1bfba112fe85d7000d75c470f15824b107f71
CreepsoOff <fgelin14@gmail.com>
2026-03-04 09:21:51 +0100

    Initial Commit

---
+++ binged/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  binged
+//
+//  Created by apprenant85 on 04/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ binged/bingedApp.swift
@@ -0,0 +1,17 @@
+//
+//  bingedApp.swift
+//  binged
+//
+//  Created by apprenant85 on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct bingedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
