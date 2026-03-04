bbe9d1aa29f2087fe828c8840286505686356004
CreepsoOff <fgelin14@gmail.com>
2026-03-04 14:04:00 +0100

    Les bozos

--- binged/ContentView.swift
+++ binged/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world")
+            Text("Bonjour c'est Fab")
         }
         .padding()
     }
