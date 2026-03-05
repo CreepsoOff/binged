bb84f69035b145e02e75ecc656a20628e1e50c38
Maindargent <ymoutiez@outlook.fr>
2026-03-05 13:09:20 +0100

    Cyriak apprend beaucoup sur r6

---
+++ binged/model/User.swift
@@ -0,0 +1,36 @@
+//
+//  User.swift
+//  binged
+//
+//  Created by Apprenant 92 on 04/03/2026.
+//
+
+import Foundation
+
+struct User: Identifiable, Hashable//, Equatable
+{
+    var id = UUID()
+    var lastName: String
+    var firstName: String
+    var email: String = ""
+    var picture: String?
+    var age: Int
+    var userBio: String?
+//  var favoriteGenre: [favoriteGenre] = []
+//    var favoriteSeries: [SeriesName?] = []
+//    var favoriteActors: [ActorName?] = []
+//    var posts: [Post?] = []
+}
+
+var magalie = User(
+    lastName: "Piquet",
+    firstName: "Magalie",
+    email: "Magalie@lecercle.mali",
+    picture: "magalie",
+    age: 70,
+    userBio: "j'adore les séries des années 1990",
+//  favoriteGenre: [.ScienceFiction, .torror, .thriller]
+//    favoriteSeries: [.PeakyBlinder, .the100, .Sherlock],
+//    favoriteActor: [.BradPit, .RobertDeniro, .EmaWatson],
+ //   posts: postsMagalie
+)
