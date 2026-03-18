9c298988c2ea82446be584d6a81b1e6a789749b2
CreepsoOff <fgelin14@gmail.com>
2026-03-18 16:38:12 +0100

    BONNE VERSION DE SEARCHSERIE

--- binged/Components/SearchSerie.swift
+++ binged/Components/SearchSerie.swift
@@ -1,5 +1,4 @@
-//
-//  SearchSerie.swift
+//  searchSerie.swift
 //  binged
 //
 //  Created by Apprenant 92 on 16/03/2026.
@@ -8,49 +7,73 @@
 import SwiftUI
 
 struct SearchSerie: View {
-    @State private var searchSerie = ""
+    // On renomme la variable d'état pour éviter le conflit avec le nom de la struct
+    @State private var searchText = ""
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     
     var user: User
-//    var allSeries: [Serie]
-//    
-//    var filteredSeries: [Serie] {
-//        let favorites = user.favoriteSerie(allSeries: allSeries)
-//                
-//                if searchSerie.isEmpty {
-//                    return favorites
-//                }
-//        return favorites.filter {
-//                    $0.name.localizedCaseInsensitiveContains(searchSerie)
-//                }
-//            }
-        
     
-//    var body: some View {
-//        VStack(alignment: .leading) {
-//            searchBar(text: $searchSerie)
-//            
-//            ScrollView(.horizontal, showsIndicators: false) {
-//                HStack {
-//                    ForEach(filteredSeries) { serie in
-//                        basicButton(Serie: serie.name)
-//                    }
-//                }
-//            }
-//            //    var filteredSeries: [Serie] {
-//            //        if searchSerie.isEmpty {
-//            //            return user.favoriteSerie
-//            //        }
-//            //        return user.favoriteSerie.filter {
-//            //            $0.name.localizedCaseInsensitiveContains(searchSerie)
-//            //        }
-//            //    }
-//        }
+    @State var vmSerie = SerieViewModels()
+    
+    @State private var listSeries: [Serie] = []
+    
+    var filteredSeries: [Serie] {
+        if searchText.isEmpty {
+
+            return listSeries
+        }
+        return listSeries.filter {
+            $0.name.localizedCaseInsensitiveContains(searchText)
+        }
+    }
     
     var body: some View {
-        SearchBar(text: $searchSerie)
-//        ForEach(filteredSeries, id: \.id) { serie in
-//            GenreButton(genre: serie.name)
-//        }
+        VStack { // J'ai ajouté un VStack pour bien séparer la searchBar du ScrollView
+            SearchBar(text: $searchText) // Modifié ici
+            
+            ScrollView(showsIndicators: false) {
+                HStack {
+                    LazyVGrid(columns: columns, spacing: 20){
+                        ForEach(filteredSeries) { serie in
+                            if let cover = serie.cover {
+                                VStack{
+                                    Image(cover)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 100, height: 150)
+                                        .clipped()
+                                        .padding(.horizontal, 4)
+                                    Text(serie.name)
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal) // Un peu de padding pour faire respirer la liste
+            }
+        }
+        .task {
+            // 1. On synchronise d'abord avec les données locales du User
+            self.listSeries = user.favoriteSeriesSafe
+            
+            // 2. Si la liste est vide mais qu'on a des IDs, on charge le reste
+            if self.listSeries.isEmpty, let ids = user.favoriteSerieIDs {
+                for serieID in ids {
+                    do {
+                        let fetchedSerie = try await vmSerie.getSerieById(serieID)
+                        if !listSeries.contains(where: { $0.id == fetchedSerie.id }) {
+                            self.listSeries.append(fetchedSerie)
+                        }
+                    } catch {
+                        print("Erreur Airtable Actor: \(error)")
+                    }
+                }
+            }
+        }
     }
 }
 
