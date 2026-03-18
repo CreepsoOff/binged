165e388e6a2400a1f3e30b1f3f8f23ca78f98ecc
Maindargent <ymoutiez@outlook.fr>
2026-03-18 10:59:32 +0100

    correction pas terminé des modèles

--- binged/Components/searchSerie.swift
+++ binged/Components/searchSerie.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct searchSerie: View {
-    @State private var searchSerie = ""
-    
+    @State private var searchSerie = ""    
     var user: User
+    var allSeries: [Serie]
     
+    var filteredSeries: [Serie] {
+        let favorites = user.favoriteSerie(allSeries: allSeries)
+                
+                if searchSerie.isEmpty {
+                    return favorites
+                }
+        return favorites.filter {
+                    $0.name.localizedCaseInsensitiveContains(searchSerie)
+                }
+            }
+        
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+                    searchBar(text: $searchSerie)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(filteredSeries) { serie in
+                                basicButton(Serie: serie.name)
+                            }
+            }
+        }
 //    var filteredSeries: [Serie] {
 //        if searchSerie.isEmpty {
 //            return user.favoriteSerie
@@ -21,12 +44,12 @@ struct searchSerie: View {
 //        }
 //    }
     
-    var body: some View {
-        searchBar(text: $searchSerie)
-//        ForEach(filteredSeries, id: \.id) { serie in
-//            genreButton(genre: serie.name)
-//        }
-    }
+//    var body: some View {
+//        searchBar(text: $searchSerie)
+////        ForEach(filteredSeries, id: \.id) { serie in
+////            SerieButton(Serie: serie.name)
+////        }
+//    }
 }
 
 #Preview {
